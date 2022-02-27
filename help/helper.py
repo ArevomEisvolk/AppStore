bbc05b104231b901c689f4c0865a3081ba3a2fdb
Åre <44285746+ArevomEisvolk@users.noreply.github.com>
2022-02-27 02:09:21 +0100

    add python3 Script helper

--- help/helper.py
+++ help/helper.py
@@ -2,7 +2,22 @@ import os
 from glob import glob
 import json
 
-for i in glob("AppStore/Apps/*/appfile.json",recursive=True):
-    appfile = json.load(open(i))
-    for var in ["title", "tagline", ""]:
-        appfile[var]
+
+def merge_two_dicts(x, y):
+    """Given two dictionaries, merge them into a new dict as a shallow copy."""
+    z = x.copy()
+    z.update(y)
+    return z
+
+li =[]
+
+for index, app in enumerate(glob("AppStore/Apps/*/appfile.json",recursive=True)):
+    with open(app, "r") as file:
+        appfile = json.load(file)
+        li.append(merge_two_dicts({"id" : index},appfile))
+
+    
+
+with open("/Users/are/Documents/Github/AreOS-Backend/config/appfile.json", "a") as af:
+    json.dump({"list" : li, "community" : [], "recommend" : []}, af)
+        
---
+++ help/test.py
@@ -0,0 +1,7 @@
+import json
+
+with open("/Users/are/Documents/Github/AreOS-Backend/config/appfile.json", "r") as file:
+	appfile = json.load(file)
+	for i in appfile.get("list"):
+		if i.get("id") == 3:
+			print(i.get("id"))
