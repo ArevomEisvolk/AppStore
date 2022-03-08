e5af6f2ce4627ccef398197d3c72b08405cb0d31
ArevomEisvolk <mario.marcel@fn.de>
2022-03-08 15:39:54 +0100

    Update helper.py

--- help/helper.py
+++ help/helper.py
@@ -1,7 +1,7 @@
 import os
 from glob import glob
 import json
-
+from pathlib import Path
 
 def merge_two_dicts(x, y):
     """Given two dictionaries, merge them into a new dict as a shallow copy."""
@@ -14,10 +14,10 @@ li =[]
 for index, app in enumerate(glob("AppStore/Apps/*/appfile.json",recursive=True)):
     with open(app, "r") as file:
         appfile = json.load(file)
-        li.append(merge_two_dicts({"id" : index},appfile))
+        li.append(merge_two_dicts({"id" : index+1},appfile))
 
     
-
-with open("/Users/are/Documents/Github/AreOS-Backend/config/appstore.json", "w") as af:
+print()
+with open(f"{Path(os.path.dirname(os.path.abspath(__file__))).parent.parent}/config/appstore.json", "w") as af:
     json.dump({"list" : li, "community" : [], "recommend" : []}, af, indent=4)
         
