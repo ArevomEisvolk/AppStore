60839a90b9c42468890d848dd8316fa99957d841
ArevomEisvolk <mario.marcel@fn.de>
2022-03-17 14:44:45 +0100

    .

--- help/category.py
+++ help/category.py
@@ -7,19 +7,10 @@ if sys.platform == "linux":
     sys.path.append("/mnt/Datengrab/python/AreOS-Backend")
 else:
      sys.path.append("/Users/are/Documents/Github/AreOS-Backend")
+from untils.text_transform import merge_two_dicts
 from untils.config import Config_Reader
 
-def merge_two_dicts(x, y):
-    """Given two dictionaries, merge them into a new dict as a shallow copy."""
-    z = x.copy()
-    z.update(y)
-    print(z)
-    return z
 
-def replace_dict_id(x,  ids):
-    """Given two dictionaries, merge them into a new dict as a shallow copy."""
-    x["id"] = ids
-    return x
 
 li = []
 is_app = set()
@@ -29,19 +20,24 @@ lu = []
 for index, app in enumerate(glob("AppStore/Apps/*/appfile.json",recursive=True)):
             with open(app, "r") as file:
                 appfile = json.load(file)
-                is_app.add("".join(appfile["category"]))
+                [is_app.add("".join(app)) for app in appfile["category"]]
 
 with open("AppStore/category-list.json", "r") as cat:
     li.append({'name': 'All', 'icon': 'apps', 'description': 'All Apps', 'id': 0,"count" : 1})
     for index, cat in enumerate(json.load(cat)):
         if cat["name"] in is_app:
             li.append(merge_two_dicts(cat,{"id":index+1, "count" : 1}))
-            lu.append({"category": cat["name"] , "id": index+1})
+        else:
+            li.append(merge_two_dicts(cat,{"id":index+1, "count" : 0}))
             
     
-a = Config_Reader("config/config.ini")
+ids = {}
+for i in li:
+    ids.update({str(i.get("id")) : i.get("name")})
+        
 
-a.write_category(str(lu))
+Config_Reader().write_value("appstore","app_ids",str(ids))
+
 
 with open(f"{Path(os.path.dirname(os.path.abspath(__file__))).parent.parent}/config/appstore/category.json", "w") as af:
     json.dump(li, af, indent=4)
